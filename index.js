0e0605af1346d93b838c625fd948653c0dbcad60
MoShak1 <161527046+MoShak1@users.noreply.github.com>
2024-12-28 17:02:00 +0330

    ad

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const bodyParser = require("body-parser");
+const isUrl = require("is-url");
 // Basic Configuration
 const port = process.env.PORT || 3000;
+let counter = 0;
+const shortendUrls = {};
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+  //check if url is valid
+
+  if (!isUrl(url)) {
+    res.send({ error: "invalid url" });
+    return;
+  }
+  counter += 1;
+  shortendUrls[counter] = url;
+  res.send({ original_url: req.body.url, short_url: counter });
+});
+
+app.get("/api/shorturl/:id", function (req, res) {
+  const id = req.params.id;
+  const url = shortendUrls[id];
+  res.redirect(url);
+});
+
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
